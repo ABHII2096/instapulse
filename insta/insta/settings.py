d6b95079f99c8a0ff9e781c7e02b7e9fff4f5899
abhi <abhikhalasi10@gmail.com>
2026-01-10 10:54:41 +0530

    Fix database config for production

--- insta/insta/settings.py
+++ insta/insta/settings.py
@@ -79,14 +79,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'insta.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import os
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        conn_max_age=600
+    )
 }
 
 
